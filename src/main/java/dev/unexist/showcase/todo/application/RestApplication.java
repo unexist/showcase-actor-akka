5ef565703b50e9ebb83374c2b3818530310ae7b6
Christoph Kappel <christoph@unexist.dev>
2024-03-07 16:28:47 +0100

    Added based class - still wip

---
+++ src/main/java/dev/unexist/showcase/todo/application/RestApplication.java
@@ -0,0 +1,68 @@
+/**
+ * @package Showcase-Actor-Akka
+ *
+ * @file Todo application
+ * @copyright 2024-present Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the Apache License v2.0.
+ * See the file LICENSE for details.
+ **/
+
+package dev.unexist.showcase.todo.application;
+
+import akka.NotUsed;
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.ActorSystem;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.server.Route;
+import dev.unexist.showcase.todo.adapter.TodoRegistry;
+import dev.unexist.showcase.todo.adapter.TodoRoutes;
+import dev.unexist.showcase.todo.domain.todo.TodoRepository;
+import dev.unexist.showcase.todo.domain.todo.TodoService;
+import dev.unexist.showcase.todo.infrastructure.persistence.ListTodoRepository;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.CompletionStage;
+
+public class RestApplication {
+    static void startHttpServer(Route route, ActorSystem<?> system) {
+        CompletionStage<ServerBinding> futureBinding =
+            Http.get(system).newServerAt("localhost", 8080).bind(route);
+
+        futureBinding.whenComplete((binding, exception) -> {
+            if (binding != null) {
+                InetSocketAddress address = binding.localAddress();
+
+                system.log().info("Server online at http://{}:{}/",
+                    address.getHostString(),
+                    address.getPort());
+            } else {
+                system.log().error("Failed to bind HTTP endpoint, terminating system", exception);
+
+                system.terminate();
+            }
+        });
+    }
+
+    public static void main(String[] args) throws Exception {
+        Behavior<NotUsed> rootBehavior = Behaviors.setup(context -> {
+            TodoRepository todoRepository = new ListTodoRepository();
+            TodoService todoService = new TodoService(todoRepository);
+
+            ActorRef<TodoRegistry.Command> todoRegistryActor =
+                context.spawn(TodoRegistry.create(todoService), "TodoRegistry");
+
+            TodoRoutes todoRoutes = new TodoRoutes(context.getSystem(), todoRegistryActor);
+
+            startHttpServer(todoRoutes.todoRoutes(), context.getSystem());
+
+            return Behaviors.empty();
+        });
+
+        ActorSystem.create(rootBehavior, "HelloAkkaHttpServer");
+    }
+}
